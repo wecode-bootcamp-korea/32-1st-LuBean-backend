573e99996497df2d2d2b129e6f7a99d71255f820
Sooyeon Im <isyqwer1@gmail.com>
2022-04-26 17:28:58 +0900

    Modify: users models.py

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=45)
+    email = models.CharField(max_length=300, unique=True)
+    password = models.CharField(max_length=300)
+    phone_number = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_tables = 'users'
