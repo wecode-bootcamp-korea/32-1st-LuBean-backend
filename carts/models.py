eff7607ceb0463a11256122792158116507baafc
Sooyeon Im <isyqwer1@gmail.com>
2022-04-27 18:45:16 +0900

    Modify: carts models.py

--- carts/models.py
+++ carts/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
 class Cart(models.Model):
-    amount  = models.PositiveIntegerField()
-    user    = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    quantity       = models.PositiveIntegerField()
+    user           = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    order          = models.ForeignKey("Order", on_delete=models.CASCADE, null=True)
+    price          = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     
     class Meta:
         db_table = 'carts'
         
-class ProductCart(models.Model):
-    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    cart    = models.ForeignKey('Cart', on_delete=models.CASCADE)
-    
+class Order(models.Model):
+    user         = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    address      = models.CharField(max_length=500, null=True)
+    order_status = models.ForeignKey("OrderStatus", on_delete=models.CASCADE)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+
     class Meta:
-        db_table = 'products_carts'
+        db_table = "orders"
+        
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "order_status"
