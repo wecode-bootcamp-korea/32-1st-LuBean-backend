14338c6f3d1b65494f41603d990d4ba12902d237
Sooyeon Im <isyqwer1@gmail.com>
2022-04-27 11:06:12 +0900

    Modify: carts models.py

--- carts/models.py
+++ carts/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Cart(models.Model):
-    amount  = models.IntegerField()
+    amount  = models.PositiveIntegerField()
     user    = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    
     class Meta:
         db_table = 'carts'
         
 class ProductCart(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     cart    = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    
     class Meta:
         db_table = 'products_carts'
