c5ad86ad59d6805d533d1729a0a28c4d966003ca
Sooyeon Im <isyqwer1@gmail.com>
2022-04-29 16:19:23 +0900

    Modify: users/utils.py

--- users/utils.py
+++ users/utils.py
@@ -10,7 +10,7 @@ def login_required(func):
         try:
             access_token = request.headers.get('Authorization', None)
             payload      = jwt.decode(access_token, SECRET_KEY, algorithms = ALGORITHM)
-            user         = User.objects.get(id=payload['id'])
+            user         = User.objects.get(id=payload['email'])
             request.user = user
         
         except jwt.InvalidSignatureError:
