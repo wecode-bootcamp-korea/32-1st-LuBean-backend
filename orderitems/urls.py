d721049fe77a851fef02cc369eb03b044b9831fd
Sooyeon Im <isyqwer1@gmail.com>
2022-05-04 18:03:59 +0900

    Deleted orderitems/urls.py

--- orderitems/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-
-# from products.views import ProductView, SortView, CountryListView, ProductDetailView
-
-# urlpatterns = [
-#     path('/', SignUpView.as_view()),
-#     path('/login', LogInView.as_view())
-# ]
